f3309d964087739c817d568060bb81c325349025
jNembhard <nembhardjl@outlook.com>
2022-07-08 13:38:33 -0400

    Audio Gear copy edit

--- components/molecules/AudioGear.tsx
+++ components/molecules/AudioGear.tsx
@@ -49,9 +49,9 @@ const AudioGear = () => {
           color="gray"
         >
           Located at the heart of New York City, Audiophile is the premier store
-          for high end headphones, earphones, speakers,and audio accessories. We
-          have a large showroom and luxury demonstration rooms available for you
-          to browse and experience a wide range of our products. Stop by our
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
           store to meet some of the fantastic people who make Audiophile the
           best place to buy your portable audio equipment.
         </Text>
